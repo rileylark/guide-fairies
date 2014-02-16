c3cbdd11583ad861b0854cd4b7cf20211fc5b4b6
Riley Eynon-Lynch <riley@peardeck.com>
2014-02-16 10:52:52 -0600

    Some comments and stuff

--- js/main.js
+++ js/main.js
@@ -2,29 +2,52 @@ require.config({
 
 });
 
-require(['guide-fairies'], function () {
+require(['guide-fairies'], function (guideFairies) {
     console.log("App loaded!");
-    var app = angular.module('guide-fairies-demo', ['guide-fairies']);
+    var app = angular.module('guide-fairies-demo', [guideFairies.name]);
 
-    app.controller("MainController", ['$scope', 'guideService', function ($scope, guideService) {
-        var guide = guideService();
-
-        guide.showStop('firstStop');
-
-        $scope.guide = guide;
-
-        $scope.showStop = function(stopName) {
-            guide.showStop(stopName);
-        }
-
-        $scope.showFairyPreferences = {
-            firstStop: true,
-            header2stop: true,
-            header3stop: true
-        };
-
-        $scope.test = "Yep, working!";
+    app.run(['demoFeaturesGuideRunner', function(featureRunner) {
+        // I think of the guide as a program that's kind of running parallel
+        // to the main functionality of the app, so I split out as much
+        // as I can from the controllers.  I think a run block is a good
+        // place to start the guide service.
+        featureRunner.start();
     }]);
+
+
+    app.controller("MainController", ['$scope', 'demoFeaturesGuideRunner', function ($scope, featureRunner) {
+        // some pollution of your controller is necessary to link the UI
+        // to the guide service.  Instead of linking functions directly
+        // like this it might be better to publish an event bus that the
+        // guide runner can listen to... maybe version 2?
+        $scope.showFeatures = featureRunner.showFeatures;
+    }]);
+
+    app.factory('demoFeaturesGuideRunner', ['$rootScope', 'guideService',
+        function ($rootScope, guideService) {
+
+            //this is the actual definition of the guide.  It includes:
+            // * The instructions that tell which fairy to go to which stop
+            // * Any extra behavior you want to add.  In my app I use the runner
+            //   to send messages to the server, and to check which stops
+            //   the user has already seen so we can avoid showing them again.
+
+            var guide = guideService();
+
+            return {
+                start: function() {
+                    guide.showStop('showFeaturesButton', 'FEATURE_FAIRY');
+                },
+
+                showFeatures: showFeatures,
+
+                showStop: guide.showStop
+            };
+
+            function showFeatures() {
+                guide.showStop('featuresHeader', 'FEATURE_FAIRY');
+            }
+        }]);
 
     angular.bootstrap(document.body, [app.name]);
 });
