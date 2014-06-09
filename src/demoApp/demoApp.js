9991f11619c4d79467ec3b0fb8e38efb9df15f1d
Riley Eynon-Lynch <riley@peardeck.com>
2014-06-09 06:38:58 -0500

    Draft copy

--- src/demoApp/demoApp.js
+++ src/demoApp/demoApp.js
@@ -5,19 +5,27 @@
     var demoApp = angular.module('FairyDemo', ['guide-fairies', 'ui.bootstrap']);
 
     demoApp.controller('MainDemoController', ['guideService', '$scope', function(guideService, $scope) {
-        guideService().showStop('showFeaturesButton', 'FEATURE_FAIRY');
+
+        var demoController = this;
+
+        guideService().showStop('showFeaturesButton', 'WELCOME_FAIRY');
 
         $scope.introModalShowing = true;
 
         $scope.hideIntroModal = function() {
             $scope.introModalShowing = false;
+            guideService().showStop('firstUsageExample', 'WELCOME_FAIRY');
         };
 
-        $scope.usageExamples = [
-            {
-                heading: 'A simple fairy to welcome users and teach them to click fairies',
-                templateUrl: 'usageExamples/simpleIntro.html'
+        $scope.openFirstUsageExample = function() {
+            demoController.firstUsageExampleOpen = true;
+        };
+
+        $scope.$watch('demoController.firstUsageExampleOpen', function(open) {
+            if (open) {
+                guideService().dismissFairy('firstUsageExample');
             }
-        ]
+        });
+
     }]);
 }());
